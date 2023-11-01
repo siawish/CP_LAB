a6b4ad4f1a4d7ef0fc118de705d2ec17b5efe691
Hafiz Malik Mohammad Siawish <146324520+siawish@users.noreply.github.com>
2023-11-01 07:14:59 -0700

    Add files via upload

---
+++ CPLAB05/task1.1.cpp
@@ -0,0 +1,15 @@
+// Print numbers in descending order.
+
+#include<iostream>
+using namespace std;
+int main() {
+	int x = 20;
+	do {
+		cout << x << endl;;
+		x--;
+
+	} while (x >= 1);
+
+
+	return 0;
+}
